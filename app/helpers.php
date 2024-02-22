2206421a84b84a99c8a5d5ebf99b2ede9e62f8cd
JP Caparas <jp@jpcaparas.com>
2024-02-22 23:52:37 +1300

    Create and test bcmath helper functions

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,27 @@ if (!function_exists('deburr')) {
         return iconv('UTF-8', 'ASCII//TRANSLIT', $string);
     }
 }
+
+if (!function_exists('decimalAdd')) {
+    /**
+     * Add two decimal numbers with precision using a BCMath function.
+     */
+    function decimalAdd($number1, $number2, $precision = 2): string
+    {
+        return bcadd($number1, $number2, $precision);
+    }
+}
+
+if (!function_exists('decimalDivide')) {
+    /**
+     * Divide two decimal numbers with precision using a BCMath function.
+     */
+    function decimalDivide($number1, $number2, $precision = 2): string
+    {
+        if ($number2 == 0) {
+            throw new InvalidArgumentException('Division by zero.');
+        }
+
+        return bcdiv($number1, $number2, $precision);
+    }
+}
