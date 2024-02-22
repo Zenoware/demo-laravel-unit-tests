0394957792facaac8977a3243c55088a221a5859
JP Caparas <jp@jpcaparas.com>
2024-02-22 23:54:09 +1300

    Write note about extending Laravel TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,11 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * @deprecated Extending this class from your test cases means you've given up trying to write a proper unit test.
+ *             Unit tests do not require the application to be bootstrapped.
+ *             When writing unit tests, ALWAYS extend PHPUnit\Framework\TestCase.
+ */
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
