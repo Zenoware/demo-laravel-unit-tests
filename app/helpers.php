f978beddab6dd593fb268c2b07bd3a2c9db3b3db
JP Caparas <jp@jpcaparas.com>
2024-02-22 23:31:12 +1300

    Type the deburr function

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,10 @@
 <?php
 
 if (!function_exists('deburr')) {
-    // Remove accents and other diacritics from a string.
-    function deburr($string)
+    /**
+     * Remove accents from a string.
+     */
+    function deburr($string): string
     {
         return iconv('UTF-8', 'ASCII//TRANSLIT', $string);
     }
